6ad33157aa5bab78c6f6a501c43bc2b677af276b
Silveryfu <silveryfu@gmail.com>
2016-10-07 13:45:40 -0700

    mod nat box

--- app/nat.sh
+++ app/nat.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-if [ "$#" -eq 0 ]; then
-echo "==> Please specify the destination IP."
+if [ "$#" -le 2 ]; then
+echo "==> Please specify the interface, source and destination IP."
 exit
 fi
 
-iptables -A PREROUTING -t nat -j DNAT --to $1
+echo 1 > /proc/sys/net/ipv4/ip_forward
+iptables -t nat -A POSTROUTING -o $1 -j MASQUERADE
+iptables -A PREROUTING -t nat -d $2 -j DNAT --to $3
